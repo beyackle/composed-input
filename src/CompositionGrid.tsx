b87e4c58d43bb6f3c1c51d62401b2f0860c12e2a
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-07-28 11:48:29 -0700

    Update CompositionGrid.tsx

--- src/CompositionGrid.tsx
+++ src/CompositionGrid.tsx
@@ -8,14 +8,32 @@ type Props = {
     id: string
 }
 
+type CompositionTree = {
+    [key: string]: string | CompositionTree
+};
+
 const gridStyle = css`
     border: 1px solid black;
+    background: #cccccc;
+`;
+
+const charPathStyle = css`
+    font-size: 16px;
+`;
+
+const outputChar = css`
+    font-size: 24px;
+`;
+
+const childMap = css`
+    display: grid;
 `;
 
 export default function CompositionGrid(props: Props) {
-    const [sequence, setSequence] = useState<string>('🆒');
-
-    console.log(compTree);
+    const [char, setChar] = useState<string>('');
+    const [sequence, setSequence] = useState<string[]>([]);
+    const [tree, setTree] = useState<CompositionTree>(compTree);
+    const [parentTree, setParentTree] = useState<CompositionTree>(compTree);
 
     return <div 
       id={props.id}
@@ -23,9 +41,28 @@ export default function CompositionGrid(props: Props) {
       tabIndex={-1}
       onKeyUp={(ev) => {
           if (ev.key === 'Enter') {
-            props.onExit(sequence);
-        }
-      }}>
-        Composition Grid
+            props.onExit(char)
+          } else if (ev.key === 'Backspace' && sequence.length > 0) {
+            setSequence(sequence.slice(0, sequence.length - 1));
+            setTree(parentTree);
+          } else if (ev.key in tree) {
+            if (typeof tree[ev.key] === 'string') {
+                setChar( tree[ev.key] as string );
+            }
+          } else if (ev.key+'+' in tree) {
+                setSequence(sequence.concat([ev.key]));
+                setParentTree(tree);
+                setTree(tree[ev.key] as CompositionTree);
+            }
+          }
+      }
+    >
+        <div id="characters">
+            {sequence.map((ch) => <span css={charPathStyle}>{ch}</span>)}
+            <div css={outputChar}>{char}</div>
+        </div>
+        <div id="children" css={childMap}>
+
+        </div>
     </div>;
 }
